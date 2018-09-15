47def95f2f07be45ff05a7e934c9bc9aa47c72f5
Wells <chawkeye@gmail.com>
2018-09-15 11:46:23 +0800

    設定分類與景點的關聯

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,7 @@
 class Category < ApplicationRecord
+  validates_presence_of :name
+
+  has_many :categories_attractions, dependent: :restrict_with_error
+  has_many :attractions, through: :categories_attractions
+  has_many :attractions
 end
