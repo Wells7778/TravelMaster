a640eef1bc3f17247e16ede38068917c1ec99801
Wells <chawkeye@gmail.com>
2018-09-15 11:17:09 +0800

    設定後台users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+
+  ROLE = {
+    normal: "一般用戶",
+    admin: "管理者"
+  }
+  def admin?
+    self.role == "admin"
+  end
 end
