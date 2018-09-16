d15854e4b28d4aab43b238f66f3d0ba9f49bf518
Kafka Tzeng <freefish0308@gmail.com>
2018-09-16 18:44:47 +0800

    attractions index and controller

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,8 +1,23 @@
 class AttractionsController < ApplicationController
   before_action :authenticate_user!
 
-
-  def index
+  def index   
+    @categories = Category.all
+    if params[:category_id]
+      @category = Category.find(params[:category_id])
+      @attractions = @category.attractions
+    else
+      @attractions = Attraction.all
+    end
   end
+
+
+
+  private
+
+    def attraction_params
+      params.require(:attraction).permit(:name, :image, :description, :address, :lat, :lng, category_ids: [])
+    end
+
 
 end
