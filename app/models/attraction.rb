f3deb195f8593b45d55098062f3484713c16b1b5
Wells <chawkeye@gmail.com>
2018-09-19 23:59:02 +0800

    設定ＴＡＧ常數

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -7,4 +7,17 @@ class Attraction < ApplicationRecord
   has_many :attractions, through: :list_attractions
 
   validates_presence_of :name, :image, :description, :address
+
+  TRAFFIC = { "BUS" => "bus",
+              "MRT" => "subway",
+              "CAR" => "driving",
+              "SCOOTER" => "bicycling",
+              "WALK" => "walking"
+  }
+  VIBE = ["情侶","家庭","戶外","低消費","刺激","散步","宗教", "restaurant"]
+  TRAVELTIME = { "半小時" => 1800,
+                 "一小時" => 3600,
+                 "兩小時" => 7200,
+                 "四小時" => 14400
+  }
 end
