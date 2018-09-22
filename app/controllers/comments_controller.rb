bccba24a29e31de9a7f9b60974647acb3b9bd8e6
Wells <chawkeye@gmail.com>
2018-09-22 21:31:48 +0800

    spot加上comment#new link

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = current_user.comments.build(comment_params)
+
+  end
+
+
+  private
+  def set_attraction
+    @attraction = Attraction.find_by(id: params[:attraction_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content, :images, :suggestion)
+  end
 end
