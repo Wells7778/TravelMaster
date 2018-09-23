e3c5492ec9bf8b810a82be4210a0fea881c0e811
Kafka Tzeng <freefish0308@gmail.com>
2018-09-23 17:36:43 +0800

    set comment controller create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+
+  def create
+    @attraction = Attraction.find(params[:attraction_id])
+    @comment = @attraction.comments.build(comment_params)
+    @comment.user = current_user
+    @comment.save!
+    redirect_to attraction_path(@attraction)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
 end
