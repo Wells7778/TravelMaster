1a2ad5e68e9b3678e665d1aeac9ab8203bcb9501
KC <weilihmen@gmail.com>
2018-09-30 11:04:58 +0800

    調整admin reviews,加入審核失敗的遊記

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -2,7 +2,7 @@ class Admin::ReviewsController < Admin::BaseController
   before_action :set_review, except: :index
   def index
     @pending_reviews = Review.where(status:"pending")
-
+    @reject_reviews = Review.where(status:"reject")
     @ransack = Review.where(status:"passed").includes(:attraction).ransack(params[:q])
     @reviews = @ransack.result(distinct: true).page(params[:page]).per(20)
   end
