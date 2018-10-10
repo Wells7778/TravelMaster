66f0e4edcb8b30e886fa86f59817f60055de2880
Wells <chawkeye@gmail.com>
2018-10-10 11:23:30 +0800

    重構attractions controller和list model

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -6,7 +6,8 @@ class AttractionsController < ApplicationController
     @comment = Comment.new
 
     #進入首頁方式0 僅顯示熱門景點
-    @attractions = Attraction.order("reviews_count desc").includes(:categories_attractions, :categories).limit(6) #attractions以熱門景點為基礎
+    @attractions = Attraction.order("reviews_count desc")
+                             .includes(:categories_attractions, :categories).limit(6) #attractions以熱門景點為基礎
     #進入首頁方式1 分類
     @categories = Category.all.order("attractions_count desc")
     if params[:category_id] && current_user
@@ -37,7 +38,6 @@ class AttractionsController < ApplicationController
 
   def search
     @comment = Comment.new
-
     #進入首頁的方法3 搜尋結果
     @way_check = 3
     # 如果無法取得裝置經緯度且為輸入地址就跳出無法搜尋
@@ -49,16 +49,16 @@ class AttractionsController < ApplicationController
       origin = search_params[:location].blank? ? "我的位置" : search_params[:location]
       # 把前端拿回來的tag分類
       # 景點分類屬於Array格式，所以先宣告
-      @vibe_tag = []
+      travel_tag = []
       search_params[:tags].split(",").each do |tag|
-        @travel_tag = Attraction::TRAFFIC[tag] if Attraction::TRAFFIC.has_key?(tag)
-        @vibe_tag << tag if Category.order("attractions_count desc").pluck(:tag_name).include?(tag)
-        @travel_time = Attraction::TRAVELTIME[tag] if Attraction::TRAVELTIME.has_key?(tag)
+        travel_mode = Attraction::TRAFFIC[tag] if Attraction::TRAFFIC.has_key?(tag)
+        travel_tag << tag if Category.order("attractions_count desc").pluck(:tag_name).include?(tag)
+        travel_time = Attraction::TRAVELTIME[tag] if Attraction::TRAVELTIME.has_key?(tag)
       end
       # 預設開車、旅行時間一小時
-      @travel_tag ||= "driving"
-      @travel_time ||= 3600
-      @list = current_user.lists.build(origin: origin, travel_time: @travel_time, travel_mode: @travel_tag, travel_tag: @vibe_tag)
+      travel_mode ||= "driving"
+      travel_time ||= 3600
+      @list = current_user.lists.build(origin: origin, travel_time: travel_time, travel_mode: travel_mode, travel_tag: travel_tag)
       # 呼叫method geocode把地址轉為經緯度
       if @list.geocode.nil?
         @list.origin_lat = search_params[:geo_location].split(",").first.to_f
@@ -67,17 +67,10 @@ class AttractionsController < ApplicationController
         @list.origin_lat = @list.geocode['lat']
         @list.origin_lng = @list.geocode['lng']
       end
-      if @list.save
-        @list.search_attraction(@vibe_tag).each do |result|
-          @list.list_attractions.create(attraction_id: result[:attraction_id], duration: result[:travel_text])
-        end
-      end
-      #以下兩個參數是Search的結果，再看後端要怎摸樣吐景點回來，複寫 @attractions 即可
-      @search_tags = @list.travel_tag
-      @search_location = @list.origin
-      # 搜尋結果直接存在list裏
+      # 搜尋結果直接存在list裏，使用回呼建立list_attraction資料
+      @list.save
       @attractions = @list.attractions.includes(:categories_attractions, :categories, :list_attractions)
-                                      .order("list_attractions.id asc")
+                                      .order("reviews_count desc").order("list_attractions.id asc")
       render :index
     end
   end
